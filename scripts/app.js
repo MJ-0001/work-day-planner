6f7c4bdd258c0b76a305151dc7300c1517530591
Matthew Jones <mejones92@gmail.com>
2021-01-15 23:03:51 +0000

    Updates added to script to include a switch statement.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,3 @@
-
-
 // Display current date and time
 var $dateAndTime = luxon.DateTime.local().toLocaleString({ 
   weekday: 'long', month: 'long', day: '2-digit', hour: '2-digit', minute: '2-digit' });
@@ -9,86 +7,163 @@ $('#currentDay').text(function() {
 })
 
 // Functions to set and get local storage
+// 9am time slot
 $("#nineStorage").click(function() {
-  var $nineConent = $('#9am').val();
-  localStorage.setItem("nine", $nineConent);
+  var $nineContent = $('#9am').val();
+  localStorage.setItem("nine", $nineContent);
 })
 
 var retrieveStorage = function() {
   $('#9am').val(localStorage.getItem("nine"));
 }
-
 retrieveStorage();
 
-// var nineAM = document.getElementById('9am');
-// var tenAM = document.getElementById('10am');
-// var elevenAM = document.getElementById('11am');
-// var twelveAM = document.getElementById('12pm');
-// var onePM = document.getElementById('1pm');
-// var twoPM = document.getElementById('2pm');
-// var threePM = document.getElementById('3pm');
-// var fourPM = document.getElementById('4pm');
-// var fivePM = document.getElementById('5pm');
+// 10am time slot
+$("#tenStorage").click(function() {
+  var $tenContent = $('#10am').val();
+  localStorage.setItem("ten", $tenContent);
+})
 
-// var nineStorage = document.getElementById('nineStorage');
-// nineStorage.addEventListener("click", function() {
-//   var nineContent = nineAM.value;  
-//   localStorage.setItem("nine", nineContent);
-// });
+var retrieveStorage = function() {
+  $('#10am').val(localStorage.getItem("ten"));
+}
+retrieveStorage();
+
+// 11am time slot
+$("#elevenStorage").click(function() {
+  var $elevenContent = $('#11am').val();
+  localStorage.setItem("eleven", $elevenContent);
+})
+
+var retrieveStorage = function() {
+  $('#11am').val(localStorage.getItem("eleven"));
+}
+retrieveStorage();
+
+// 12pm time slot
+$("#twelveStorage").click(function() {
+  var $twelveContent = $('#12pm').val();
+  localStorage.setItem("twelve", $twelveContent);
+})
+
+var retrieveStorage = function() {
+  $('#12pm').val(localStorage.getItem("twelve"));
+}
+retrieveStorage();
+
+// 1pm time slot
+$("#oneStorage").click(function() {
+  var $oneContent = $('#1pm').val();
+  localStorage.setItem("one", $oneContent);
+})
+
+var retrieveStorage = function() {
+  $('#1pm').val(localStorage.getItem("one"));
+}
+retrieveStorage();
+
+// 2pm time slot
+$("#twoStorage").click(function() {
+  var $twoContent = $('#2pm').val();
+  localStorage.setItem("two", $twoContent);
+})
+
+var retrieveStorage = function() {
+  $('#2pm').val(localStorage.getItem("two"));
+}
+retrieveStorage();
+
+// 3pm time slot
+$("#threeStorage").click(function() {
+  var $threeContent = $('#3pm').val();
+  localStorage.setItem("three", $threeContent);
+})
+
+var retrieveStorage = function() {
+  $('#3pm').val(localStorage.getItem("three"));
+}
+retrieveStorage();
+
+// 4pm time slot
+$("#fourStorage").click(function() {
+  var $fourContent = $('#4pm').val();
+  localStorage.setItem("four", $fourContent);
+})
+
+var retrieveStorage = function() {
+  $('#4pm').val(localStorage.getItem("four"));
+}
+retrieveStorage();
+
+// 5pm time slot
+$("#fiveStorage").click(function() {
+  var $fiveContent = $('#5pm').val();
+  localStorage.setItem("five", $fiveContent);
+})
+
+var retrieveStorage = function() {
+  $('#5pm').val(localStorage.getItem("five"));
+}
+retrieveStorage();
+
+// Check if the current time equals one of the hours in the planner
+var currentTime = function() {  
+  var $timeSlice = $dateAndTime.slice(-5);
+  var $time = $timeSlice.charAt(0) + $timeSlice.charAt(1);
+  switch($time) {
+    case "09":
+      $("#9am").css("background-color", "#BC243C");
+      break;
+    case "10":
+      $("#10am").css("background-color", "#BC243C");
+      break;
+    case "11":
+      $("#11am").css("background-color", "#BC243C");
+      break;
+    case "12":
+      $("#12pm").css("background-color", "#BC243C");
+      break;
+    case "13":
+      $("#1pm").css("background-color", "#BC243C");
+      break;
+    case "14":
+      $("#2pm").css("background-color", "#BC243C");
+      break;
+    case "15":
+      $("#3pm").css("background-color", "#BC243C");
+      break;
+    case "16":
+      $("#4pm").css("background-color", "#BC243C");
+      break;
+    case "17":
+      $("#5pm").css("background-color", "#BC243C");
+      break;
+  }
+}
+currentTime();
 
 
-// var tenStorage = document.getElementById('tenStorage');
-// var getTen = function() {
-//   var tenContent = tenAM.textContent;
-//   localStorage.setItem("ten", tenContent);
+
+// var currentTime = function() {  
+//   var $time = $dateAndTime.slice(-5);
+//   if ($time.charAt(0) === '0' && $time.charAt(1) === '9') {
+//     $("#9am").css("background-color", "#BC243C");
+//   } else if ($time.charAt(0) === '1' && $time.charAt(1) === '0') {
+//     $("#10am").css("background-color", "#BC243C");
+//   } else if ($time.charAt(0) === '1' && $time.charAt(1) === '1') {
+//     $("#11am").css("background-color", "#BC243C");
+//   } else if ($time.charAt(0) === '1' && $time.charAt(1) === '2') {
+//     $("#12pm").css("background-color", "#BC243C");
+//   } else if ($time.charAt(0) === '1' && $time.charAt(1) === '3') {
+//     $("#1pm").css("background-color", "#BC243C");
+//   } else if ($time.charAt(0) === '1' && $time.charAt(1) === '4') {
+//     $("#2pm").css("background-color", "#BC243C");
+//   } else if ($time.charAt(0) === '1' && $time.charAt(1) === '5') {
+//     $("#3pm").css("background-color", "#BC243C");
+//   } else if ($time.charAt(0) === '1' && $time.charAt(1) === '6') {
+//     $("#4pm").css("background-color", "#BC243C");
+//   } else if ($time.charAt(0) === '2' && $time.charAt(1) === '2') {
+//     $("#5pm").css("background-color", "#BC243C");
+//   }
 // }
-// tenStorage.addEventListener("click", getTen);
-
-// var elevenStorage = document.getElementById('elevenStorage');
-// var getEleven = function() {
-//   var elevenContent = elevenAM.textContent;
-//   localStorage.setItem("eleven", elevenContent);
-// }
-// elevenStorage.addEventListener("click", getEleven);
-
-// var twelveStorage = document.getElementById('twelveStorage');
-// var getTwelve = function() {
-//   var twelveContent = twelveAM.textContent;
-//   localStorage.setItem("twelve", twelveContent);
-// }
-// twelveStorage.addEventListener("click", getTwelve);
-
-// var oneStorage = document.getElementById('oneStorage');
-// var getOne = function() {
-//   var oneContent = oneAM.textContent;
-//   localStorage.setItem("one", oneContent);
-// }
-// oneStorage.addEventListener("click", getOne);
-
-// var twoStorage = document.getElementById('twoStorage');
-// var getTwo = function() {
-//   var twoContent = twoAM.textContent;
-//   localStorage.setItem("two", twoContent);
-// }
-// twoStorage.addEventListener("click", getTwo);
-
-// var threeStorage = document.getElementById('threeStorage');
-// var getThree = function() {
-//   var threeContent = threeAM.textContent;
-//   localStorage.setItem("three", threeContent);
-// }
-// threeStorage.addEventListener("click", getThree);
-
-// var fourStorage = document.getElementById('fourStorage');
-// var getFour = function() {
-//   var fourContent = fourAM.textContent;
-//   localStorage.setItem("four", fourContent);
-// }
-// fourStorage.addEventListener("click", getFour);
-
-// var fiveStorage = document.getElementById('fiveStorage');
-// var getFive = function() {
-//   var fiveContent = fiveAM.textContent;
-//   localStorage.setItem("five", fiveContent);
-// }
-// fiveStorage.addEventListener("click", getFive);
+// currentTime();
